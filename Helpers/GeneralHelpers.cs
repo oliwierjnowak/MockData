54c2b08f243b98afa09eb0b1b9df5e6bef89c4b6
Oliwier Nowak <NOW20328@spengergasse.at>
2023-03-28 23:12:46 +0200

    new features

--- Helpers/GeneralHelpers.cs
+++ Helpers/GeneralHelpers.cs
@@ -5,12 +5,20 @@ namespace MockData.Helpers
     public class GeneralHelpers
     {
         static int count = 0;
+
+       
         //Oliwier Nowak
         public string GenerateEmail(string name)
         {
-            var split = name.Split(' ');
+            var split = name.ToLower().Split(' ');
             var email = split[0] +"@"+ split[1]+".at";
 
+
+        
+
+
+
+            
             return Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$") ? email : $"aa{count}@gmail.com";
       
         }
