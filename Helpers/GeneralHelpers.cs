e80f51ec2d8a7f108a206d9a0b13ecac3360f474
onow <oliwierjnowak@gmail.com>
2023-03-28 11:56:47 +0200

    new f

---
+++ Helpers/GeneralHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MockData.Helpers
+{
+    public class GeneralHelpers
+    {
+        static int count = 0;
+        //Oliwier Nowak
+        public string GenerateEmail(string name)
+        {
+            var split = name.Split(' ');
+            var email = split[0] +"@"+ split[1]+".at";
+
+            return Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$") ? email : $"aa{count}@gmail.com";
+      
+        }
+    }
+}
